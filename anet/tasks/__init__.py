04e07fcc64e6c431a2231d04832e429e24ce1eb0
Thomas A. Unger <69393628+thomasaunger@users.noreply.github.com>
2021-03-28 15:35:37 +0200

    Redesign MNIST

--- anet/tasks/__init__.py
+++ anet/tasks/__init__.py
@@ -1,1 +1,26 @@
+from gym.envs.registration import register
 
+register(
+    id="MNISTEnv-v0",
+    entry_point="anet.tasks.mnist.envs:MNISTEnv",
+)
+
+register(
+    id="MNISTEnvBinary-v0",
+    entry_point="anet.tasks.mnist.envs:MNISTEnvBinary",
+)
+
+register(
+    id="MNISTEnvQuaternary-v0",
+    entry_point="anet.tasks.mnist.envs:MNISTEnvQuaternary",
+)
+
+register(
+    id="MNISTEnvSenary-v0",
+    entry_point="anet.tasks.mnist.envs:MNISTEnvSenary",
+)
+
+register(
+    id="MNISTEnvOctonary-v0",
+    entry_point="anet.tasks.mnist.envs:MNISTEnvOctonary",
+)
